0605a913cc158a61aafd36f1f86c2f4b176bd0f8
atena kalaki <atena.kalaki@gmail.com>
2020-04-09 16:43:26 +1000

    Mvc Rest Controllet Test Using TestRestTemplate

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTestIT.java
@@ -0,0 +1,25 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BeerControllerTestIT {
+
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void listBeers() {
+
+        BeerPagedList beerPagedList = testRestTemplate.getForObject("/api/v1/beer",BeerPagedList.class);
+        assertThat(beerPagedList.getContent()).hasSize(3);
+    }
+}
